1d18a467995f74840a3b9ccbaa681577106180eb
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-09-16 23:41:10 +0400

    Adds CRUD examples

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/widgets/header.dart';
 import 'package:fluttershare/widgets/progress.dart';
+
+final usersRef = Firestore.instance.collection('users');
+//final userRef = Firestore.instance.collection('users').document()
 
 class Timeline extends StatefulWidget {
   @override
@@ -8,11 +12,107 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+  List<dynamic> users = [];
+
+  @override
+  void initState() {
+    deleteUser();
+    createUser();
+    updateUser();
+    getUsers();
+    super.initState();
+  }
+
+  deleteUser() async {
+    final doc = await usersRef.document("asdafffddd").get();
+    if (doc.exists) {
+      usersRef.document().delete();
+    }
+  }
+
+  createUser() async {
+    final doc = await usersRef.document("asdafffddd").get();
+    if (!doc.exists) {
+      usersRef.document("asdafffddd").setData({
+        "username": "Jeff",
+        "postsCount": 0,
+        "isAdmin": false,
+      });
+    }
+  }
+
+  updateUser() async {
+    final doc = await usersRef.document("asdafffd").get();
+    if (doc.exists) {
+      doc.reference.updateData({
+        "username": "Jonh",
+        "postsCount": 33,
+        "isAdmin": false,
+      });
+    }
+  }
+
+  // getUserById() async {
+  //   final String id = '6MT7Q2E40s0qgAWT0wG3';
+  //   DocumentSnapshot doc = await usersRef.document(id).get();
+  //   print(doc.documentID);
+  //   print(doc.data);
+  //   print(doc.exists);
+  //   // .then((DocumentSnapshot doc) {
+  //   //    print(doc.documentID);
+  //   //    print(doc.data);
+  //   //    print(doc.exists);
+  //   // });
+  // }
+
+  getUsers() async {
+    final QuerySnapshot snapshot = await usersRef
+        // .where("postCount", isGreaterThan: 2)
+        // .where("username", isEqualTo: "Jane")
+        // .orderBy("postCount")
+        .getDocuments();
+
+    setState(() {
+      users = snapshot.documents;
+    });
+    snapshot.documents.forEach((DocumentSnapshot doc) {
+      // print(doc.documentID);
+      // print(doc.data);
+      // print(doc.exists);
+    });
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
       appBar: header(context, isAppTitle: true),
-      body: linearProgress(),
+      body: StreamBuilder(
+          stream: usersRef.snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return circularProgress();
+            }
+
+            final children = snapshot.data.documents
+                .map<Widget>((data) => _buildListItem(data))
+                .toList();
+
+            return Container(
+              child: ListView(children: children),
+            );
+          }),
+      // body: Container(
+      //   child: ListView(
+      //     children: users.map((user) => Text(user['username'])).toList(),
+      //   ),
+      // ),
+    );
+  }
+
+  Widget _buildListItem(dynamic data) {
+    return new ListTile(
+      title: new Text(data['username']),
+      subtitle: new Text("Class"),
     );
   }
 }
